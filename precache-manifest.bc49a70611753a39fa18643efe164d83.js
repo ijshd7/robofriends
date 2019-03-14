18110c13c7997c5dd62bbbc9ec645bf9d3d9c758
Isaiah <ijshd7@mail.missouri.edu>
2019-03-14 01:34:37 -0500

    Updates

---
+++ precache-manifest.bc49a70611753a39fa18643efe164d83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "b763d672cee6571fb697",
+    "url": "/robofriends/static/js/main.fe517be3.chunk.js"
+  },
+  {
+    "revision": "5b6febb4c86ffe044619",
+    "url": "/robofriends/static/js/2.7cb68033.chunk.js"
+  },
+  {
+    "revision": "b763d672cee6571fb697",
+    "url": "/robofriends/static/css/main.2d927cad.chunk.css"
+  },
+  {
+    "revision": "5b6febb4c86ffe044619",
+    "url": "/robofriends/static/css/2.b71c3f4d.chunk.css"
+  },
+  {
+    "revision": "2e7d65a7bc9a26f1964243f79d802db0",
+    "url": "/robofriends/index.html"
+  }
+];
